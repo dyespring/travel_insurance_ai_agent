072ff67f0bcd3735a8af9d9767875bb168e762b8
jyzhang660 <jyzhang660@163.com>
2025-05-08 10:21:46 +1000

    add postgreSQL connecter logic

---
+++ function1/db_connector.py
@@ -0,0 +1,26 @@
+import os
+import psycopg2
+import redis
+from dotenv import load_dotenv
+
+load_dotenv()
+
+POSTGRES_DB = os.getenv('POSTGRES_DB')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT')
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = int(os.getenv('REDIS_PORT'))
+
+def get_postgres_connection():
+    return psycopg2.connect(
+        dbname=POSTGRES_DB,
+        user=POSTGRES_USER,
+        password=POSTGRES_PASSWORD,
+        host=POSTGRES_HOST,
+        port=POSTGRES_PORT
+    )
+
+def get_redis_client():
+    return redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0)
